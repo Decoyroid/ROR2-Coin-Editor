057de4510f8699e2ea6a42d7cd32ddec00378c97
Decoyroid <74211871+Decoyroid@users.noreply.github.com>
2021-09-01 00:33:02 -0400

    Update ROR2 Lunar Coin Editor.py

--- ROR2 Lunar Coin Editor.py
+++ ROR2 Lunar Coin Editor.py
@@ -7,6 +7,7 @@ import xml.etree.ElementTree as ET
 
 def main():
 
+    #Declarations
     fullpath = []
     profiles = []
     count = 0
@@ -22,40 +23,39 @@ def main():
 
     path = root + '/632360/remote/UserProfiles/'
 
-
     for filename in os.listdir(path):
         if not filename.endswith('.xml'): continue
         file = md.parse(path + filename)
         fullpath.append(path + filename)
         profiles.append(file.getElementsByTagName('name')[ 0 ].childNodes[ 0 ].nodeValue)
     
+    #Input time :-)
     print('Choose profile to edit:\n')
 
     for number, letter in enumerate(profiles):
         print(number, letter)
     print()
     
-
-    
     #Get the file name
     profInp = input('Enter a number: ')
     while int(profInp) < 0 or int(profInp) >= len(profiles):
         print('Invalid')
         print('Choose profile to edit:\n')
-
         for number, letter in enumerate(profiles):
             print(number, letter)
         print()
-
         profInp = input('Enter a number: ')
 
+    #Parse selected profile
     file = md.parse(fullpath[int(profInp)])
     
+    #Ask coins
     coinsIn = input('Enter coin amount: ')
     while int(coinsIn) < 0:
         print("Please do 0 or more :-):")
         coinsIn = input('Enter coin amount: ')
     
+    #Edit
     file.getElementsByTagName( "coins" )[ 0 ].childNodes[ 0 ].nodeValue = str(coinsIn)
 
     with open( fullpath[int(profInp)], "w" ) as fs: 
@@ -65,12 +65,7 @@ def main():
 
     print("Enjoy your coins. Thanks for using :-). - Decoyroid")
     os.system('pause')
+    #Goodbye
 
 if __name__ == '__main__':
     main()
-
-
-    
-    
-
-
